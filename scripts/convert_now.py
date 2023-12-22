b19b830469c1cc7b1c2ca15bb5276edfdd71b0ea
validcube <pun.butrach@gmail.com>
2023-12-22 20:09:10 +0700

    feat(IMT): additional output in PNG format
    Signed-off-by: validcube <pun.butrach@gmail.com>

--- scripts/convert_now.py
+++ scripts/convert_now.py
@@ -7,3 +7,4 @@ for file in os.listdir("assets"):
   img = Image.open(os.path.join("assets", file))
   img_rounded_corners = round_corners(img=img, corner_radius=24)
   export_to_webp(img_rounded_corners, os.path.join("output", f"{file.strip('.png')}.webp"), "WEBP")
+  export_to_webp(img_rounded_corners, os.path.join("output", f"{file.strip('.png')}.png"), "PNG")
